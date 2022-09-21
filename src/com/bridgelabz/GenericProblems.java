026830087f4868ad35c5370585163e0d2372d04a
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-21 13:47:46 +0530

    Added:Usecase 2 for Day13 problems here we are checking the maximum value of the given float number

--- src/com/bridgelabz/GenericProblems.java
+++ src/com/bridgelabz/GenericProblems.java
@@ -13,17 +13,15 @@ import java.util.Scanner;
 
 
 /*
- * Given 3 Integers find the maximum
+ * Given 3 Float find the maximum
 - Ensure to test code with the Test Case.
-- To ensure your Code works you need 3 test cases with Max Number at 1st, 2nd and 3rd
-- Use Integer Object and compareTo
-UC 1 method to test the maximum number
+- UC 2 Case
  */
 public class GenericProblems {
 	
-	// creating method to find max for Integer
-			public static void findMax(Integer a ,Integer b,Integer c) {
-				Integer	max =  a;
+	// creating method to find max for Float
+			public static void findMax(Float a ,Float b,Float c) {
+				Float	max =  a;
 				if(b.compareTo(a)>0) {
 					max = b;
 				}
@@ -36,9 +34,9 @@ public class GenericProblems {
 	
 		
 	 public static void main(String[] args) {
-		 Integer a = 100;
-		 Integer b = 150;
-		 Integer c = 30;
+		 Float a = 100.6f;
+		 Float b = 150.4f;
+		 Float c = 30.6f;
 		 findMax(a, b, c);
 		 
 		 
