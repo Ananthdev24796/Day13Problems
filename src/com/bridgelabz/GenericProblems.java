95a1a447132a11ad66a51a9134a21079ae1261c6
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-21 13:35:29 +0530

    Added:UseCase 1 for Day 13 problem

--- src/com/bridgelabz/GenericProblems.java
+++ src/com/bridgelabz/GenericProblems.java
@@ -3,18 +3,46 @@
  */
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 /**
  * @author Iam_A
  *
  */
+
+
+
+/*
+ * Given 3 Integers find the maximum
+- Ensure to test code with the Test Case.
+- To ensure your Code works you need 3 test cases with Max Number at 1st, 2nd and 3rd
+- Use Integer Object and compareTo
+UC 1 method to test the maximum number
+ */
 public class GenericProblems {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	// creating method to find max for Integer
+			public static void findMax(Integer a ,Integer b,Integer c) {
+				Integer	max =  a;
+				if(b.compareTo(a)>0) {
+					max = b;
+				}
+				if(c.compareTo(a)>0) {
+					max =c;
+				}
+				System.out.println(max);
+				
+			}
+	
+		
+	 public static void main(String[] args) {
+		 Integer a = 100;
+		 Integer b = 150;
+		 Integer c = 30;
+		 findMax(a, b, c);
+		 
+		 
+		 
+		
 	}
-
 }
